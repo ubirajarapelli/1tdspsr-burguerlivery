67d748394fa926a28bd3d998482b4bf2530bb9e2
Gabriel-Souza10 <Gabriel292603@gmail.com>
2025-03-25 22:59:33 -0300

    veja com carinho

---
+++ app/components/header.tsx
@@ -0,0 +1,32 @@
+import Link from "next/link";
+
+
+export default function Header() {
+    
+    return (
+    <div className="flex flex-row bg-pink-500 text-white">
+        
+        <header className="py-6 text-2xl flex flex-row items-center justify-between w-full">
+
+          <a className="text-4xl ml-4">Burguerdelivery</a>
+
+          <nav className="mt-4 flex flex-row gap-4 mr-14 ml-auto space-y-2">
+            <Link href="" legacyBehavior>
+              <a className="hover:text-black">Hamburgueres</a>
+            </Link>
+            <Link href="" legacyBehavior>
+              <a className="hover:text-black">Entrada</a>
+            </Link>
+            <Link href="/sobremesas" legacyBehavior>
+              <a className="hover:text-black">Sobremesas</a>
+            </Link>
+            <Link href="/bebidas" legacyBehavior>
+              <a className="hover:text-black">Bebidas</a>
+            </Link>
+          </nav>
+
+        </header>
+      </div>
+      
+    );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+
 export default function Home() {
   return (
     <div className="container mx-auto">
