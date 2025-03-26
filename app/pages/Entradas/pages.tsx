7f8ae6f774e05b4dde4ac97dfdf71b320f44a1f2
Laboratório FIAP <labsfiap@fiap.com.br>
2025-03-25 23:08:41 -0300

    Subindo a minha versão

---
+++ app/pages/Entradas/pages.tsx
@@ -0,0 +1,15 @@
+import Header from "@/app/components/Header/Header";
+import Footer from "@/app/components/Footer/Footer";
+
+export default function Entradas() {
+return (
+<div>
+<Header />
+<main className="p-4">
+<h1>Nossas Entradas</h1>
+<p>Confira nossa seleção de Entradas deliciosas!</p>
+</main>
+<Footer />
+</div>
+);
+}
