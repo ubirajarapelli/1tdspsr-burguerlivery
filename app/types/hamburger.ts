72009775225fcad0290b93c30afa03910fa971de
Natalia.o.santos <natalia.o.santos.00@gmail.com>
2025-04-26 14:46:25 -0300

    adicionando o type do gamburguer

---
+++ app/types/hamburger.ts
@@ -0,0 +1,14 @@
+export type Hamburger = {
+  id: number
+  image: string[]
+  title: string
+  description: string
+  values: {
+    single: number
+    combo: number
+  }
+}
+
+export type HamburgerList = Hamburger[]
+
+  
