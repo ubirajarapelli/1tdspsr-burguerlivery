3235bace71afa16632b3265cd3263b87b2869c26
fiapfelipe <rm559175@fiap.com.br>
2025-04-26 00:30:28 -0300

    Tipo burguer

---
+++ app/types/burguer.ts
@@ -0,0 +1,12 @@
+export type Burguer = {
+    id: number
+    image: string
+    title: string
+    description: string
+    values: {
+        single: number
+        combo: number
+    }
+}
+
+export type BurguerList = Burguer[]
