3f2664b1a06541632a1c0977d86617f5bf448319
Felipe Damasceno <patcho.olec@gmail.com>
2025-04-26 00:47:32 -0300

    adding types in the code

---
+++ app/types/beverage.ts
@@ -0,0 +1,10 @@
+export type Beverage = {
+    id: number
+    image: string
+    title: string
+    description: string
+    value: number
+  }
+  
+  export type BeverageList = Beverage[]
+  
---
+++ app/types/hamburguer.ts
@@ -0,0 +1,13 @@
+export type Hamburger = {
+    id: number
+    image: string[]
+    title: string
+    description: string
+    values: {
+      single: number
+      combo: number
+    }
+  }
+  
+  export type HamburgerList = Hamburger[]
+  
