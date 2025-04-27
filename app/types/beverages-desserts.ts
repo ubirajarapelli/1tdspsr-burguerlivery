d4c3eeee26e00b4ded8077c740dab643e09965ef
Alex Maia <alex.ribeiro.maiaa@gmail.com>
2025-04-27 20:57:08 -0300

    refactor: otimizating type equals

---
+++ app/types/beverages-desserts.ts
@@ -0,0 +1,9 @@
+export type beverages_desserts = {
+    id: number
+    image: string
+    title: string
+    description: string
+    value: number
+}
+
+export type beverages_dessertsList = beverages_desserts[]
