c88808bbbb019dc326fb0ed202926177df13a7c8
julialima7 <julia.lrodrigues7@gmail.com>
2025-04-27 17:37:46 -0300

    context

--- app/context/orderContext.tsx
+++ app/context/orderContext.tsx
@@ -1,31 +1,49 @@
 "use client"
 import { createContext, useEffect, useState } from "react"
-
+ 
 interface OrderProviderProps {
   children: React.ReactNode
 }
-
+ 
 const OrderContext = createContext<unknown>({})
-
+ 
 // Provedor de contexto
 // O Provedor de contexto é um componente que envolve a aplicação e fornece o contexto para todos os componentes filhos
 export const OrderProvider = ({ children }: OrderProviderProps) => {
   const [appetizerOrder, setAppetizerOrder] = useState<number[]>([])
-  const [hamburgerOrder, setHamburgerOrder] = useState<number[]>([])
-
+  const [burguerOrder, setBurguerOrder] = useState<number[]>([])
+  const [dessertOrder, setDessertOrder] = useState<number[]>([])
+  const [beverageOrder, setBeverageOrder] = useState<number[]>([])
+ 
   const [totalItems, setSetTotalItems] = useState<number>(0)
-
+ 
   useEffect(() => {
     setSetTotalItems(appetizerOrder.length)
   }, [appetizerOrder])
-
+ 
+  useEffect(() => {
+    setSetTotalItems(burguerOrder.length)
+  }, [burguerOrder])
+ 
+  useEffect(() => {
+    setSetTotalItems(dessertOrder.length)
+  }, [dessertOrder])
+ 
+  useEffect(() => {
+    setSetTotalItems(beverageOrder.length)
+  }, [beverageOrder])
+ 
   return (
     <OrderContext.Provider
       value={{
         appetizerOrder,
         setAppetizerOrder,
-        hamburgerOrder,
-        setHamburgerOrder,
+        burguerOrder,
+        setBurguerOrder,
+        dessertOrder,
+        setDessertOrder,
+        beverageOrder,
+        setBeverageOrder,
         totalItems,
         setSetTotalItems,
       }}
@@ -34,5 +52,5 @@ export const OrderProvider = ({ children }: OrderProviderProps) => {
     </OrderContext.Provider>
   )
 }
-
+ 
 export default OrderContext
