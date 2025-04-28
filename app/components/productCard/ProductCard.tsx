ff984f96bfe8d56896705b3a4b2e6da758123a2d
GuilhermeJun <guilherme.jun@hotmail.com>
2025-04-27 23:17:37 -0300

    componente productCard atualizado

--- app/components/productCard/ProductCard.tsx
+++ app/components/productCard/ProductCard.tsx
@@ -63,3 +63,34 @@ interface ProductCardActionProps {
 export const ProductCardAction = ({ children }: ProductCardActionProps) => {
   return <div className="flex flex-col gap-2">{children}</div>
 }
+
+interface ProductCardValueProps {
+  //children: React.ReactNode
+  id: string
+  value: number
+  onChange: (event: React.ChangeEvent<HTMLInputElement>) => void
+}
+export const ProductCardValue = ({
+  id, value, onChange
+}: ProductCardValueProps) => {
+  const formatCurrency = (value: number) => {
+    return value.toLocaleString("pt-BR", {
+      style: "currency",
+      currency: "BRL",
+    })
+  }
+  return (
+    <div className="flex flex-row-reverse items-center justify-between p-2 bg-gray-100 rounded-lg text-gray-700 mb-2">
+    <input
+      type="checkbox"
+      id={id}
+      value={value}
+      onChange={onChange}
+      className="h-4 w-4 text-amber-600 focus:ring-amber-600 border-amber-400"
+    />
+    <label htmlFor={id} className="w-full text-sm cursor-pointer">
+      {formatCurrency(value)}
+    </label>
+  </div>
+  )
+}
