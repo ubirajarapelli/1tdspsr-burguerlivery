9ebe3387ba2ec7283a9f01ba3cd58df0f235052a
Henrique <hbshenrique.ti@gmail.com>
2025-04-27 23:46:53 -0300

    Tipando as variaveis

---
+++ app/types/desserts.ts
@@ -0,0 +1,9 @@
+export type desserts = {
+    id: number,
+    image: string,
+    title: string,
+    description: string,
+    value: number,
+  }
+
+export type dessertsList = desserts[]
---
+++ app/types/hamburger.ts
@@ -0,0 +1,14 @@
+export type Hamburger = {
+    id: number,
+    image: Array<string>
+    title: string,
+    description: string
+    values: {
+      single: number,
+      combo: number,
+    },
+  }
+
+  
+  export type HamburgerList = Hamburger[]
+  
