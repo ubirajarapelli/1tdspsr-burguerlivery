1302118d9a060f2aca3b6d670932f57079dbd197
GuilhermeJun <guilherme.jun@hotmail.com>
2025-05-25 04:30:53 -0300

    Tela de Entradas atualizado

--- app/pages/entradas/page.tsx
+++ app/pages/entradas/page.tsx
@@ -16,7 +16,8 @@ import {
 import { Appetizer, AppetizerList } from "@/app/types/appetizer"
 
 export default function Appetizers() {
-  const baseURL = process.env.NEXT_PUBLIC_API_URL
+  const baseURL = "https://burgerlivery-api.vercel.app"
+  //process.env.NEXT_PUBLIC_API_URL
 
   const { appetizerOrder, setAppetizerOrder } = useContext(
     OrderContext
